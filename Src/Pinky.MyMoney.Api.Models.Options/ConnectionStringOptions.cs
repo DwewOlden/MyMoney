649c0246f403407c85710e541d3587bd9d605074
Andrew Olden <Andrew.Olden@outlook.com>
2024-08-24 17:46:22 +0100

    Another few projects, and moves them into the  correct locations

---
+++ Src/Pinky.MyMoney.Api.Models.Options/ConnectionStringOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinky.MyMoney.Api.Models.Options
+{
+    /// <summary>
+    /// Connection string from the app settings.
+    /// </summary>
+    public class ConnectionStringOptions
+    {
+        public const string SectionName = "ConnectionStrings";
+        public string? MyMoney {  get; set; }
+    }
+}
